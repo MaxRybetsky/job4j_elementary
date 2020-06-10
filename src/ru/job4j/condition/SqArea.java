0c3738e884bcb8897836e3c4e5f59e30b32bd237
MaxRybetsky <stsdewar@ya.ru>
2020-06-10 22:09:43 +0300

    9.2. Checkstyle

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -3,8 +3,8 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(int p, int k) {
         double h = p / (2 * (k + 1));
-        double l = h * k;
-        double rsl = l * h;
+        double L = h * k;
+        double rsl = L * h;
         return rsl;
     }
 
