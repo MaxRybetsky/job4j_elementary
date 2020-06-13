09aa76ca7cd93645e2a43565940f58a2071fc0eb
MaxRybetsky <stsdewar@ya.ru>
2020-06-13 13:20:50 +0300

    6.7.3. Массив из диагонали матрицы.

--- test/ru/job4j/array/MatrixCheckTest.java
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -42,6 +43,19 @@ public class MatrixCheckTest {
         assertThat(
                 MatrixCheck.monoVertical(input, 2),
                 is(true)
+        );
+    }
+
+    @Test
+    public void whenDiagonal() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'},
+        };
+        assertThat(
+                MatrixCheck.extractDiagonal(input),
+                is(new char[] {'X', 'X', 'X'})
         );
     }
 }
